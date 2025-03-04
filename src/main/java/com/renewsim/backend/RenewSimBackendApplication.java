32155e246eab3e89f4a9033a6553ad6b9642aeb2
LannyRivero <lannyrivero288@gmail.com>
2025-03-04 15:23:53 +0100

    Setup: Add Spring Boot backend structure

---
+++ src/main/java/com/renewsim/backend/RenewSimBackendApplication.java
@@ -0,0 +1,13 @@
+package com.renewsim.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenewSimBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenewSimBackendApplication.class, args);
+	}
+
+}
