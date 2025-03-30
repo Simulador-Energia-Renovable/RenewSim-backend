99db8c0b036d11a573cc332df3dfb37e2190d200
lannyRivero <lannyrivero288@gmail.com>
2025-03-30 18:35:02 +0200

    fix: correct prop name and add null check

--- src/main/java/com/renewsim/backend/simulation/SimulationController.java
+++ src/main/java/com/renewsim/backend/simulation/SimulationController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/simulacion")
+@RequestMapping("/api/v1/simulation")
 @CrossOrigin(origins = "http://localhost:5173") 
 public class SimulationController {
 
