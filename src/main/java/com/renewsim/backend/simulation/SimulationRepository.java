427fb674b1f8bc9c0eb0e216584c0bd497d25582
lannyRivero <lannyrivero288@gmail.com>
2025-03-30 17:45:14 +0200

    feat: create SimulationRepository with findAllByUser method

---
+++ src/main/java/com/renewsim/backend/simulation/SimulationRepository.java
@@ -0,0 +1,11 @@
+package com.renewsim.backend.simulation;
+
+import org.springframework.stereotype.Repository;
+import com.renewsim.backend.user.User;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface SimulationRepository extends JpaRepository<Simulation, Long> {
+    List<Simulation> findAllByUser(User user);
+}
