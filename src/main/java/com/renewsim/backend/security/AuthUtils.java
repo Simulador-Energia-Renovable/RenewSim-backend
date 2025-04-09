562e176552047465ff97738ad4ceaea7b4d1dc67
lannyRivero <lannyrivero288@gmail.com>
2025-04-09 23:10:53 +0200

    feat(exception): handle unauthorized access globally with custom exception

--- src/main/java/com/renewsim/backend/security/AuthUtils.java
+++ src/main/java/com/renewsim/backend/security/AuthUtils.java
@@ -1,20 +1,21 @@
 package com.renewsim.backend.security;
 
-
 import com.renewsim.backend.user.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import com.renewsim.backend.exception.UnauthorizedException;
 
 public class AuthUtils {
 
-    private AuthUtils() {  
+    private AuthUtils() {
+
     }
 
     public static User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || !authentication.isAuthenticated()) {
-            throw new RuntimeException("No authenticated user found");
+            throw new UnauthorizedException("User is not authenticated");
         }
 
         Object principal = authentication.getPrincipal();
@@ -22,8 +23,7 @@ public class AuthUtils {
         if (principal instanceof UserDetailsImpl userDetails) {
             return userDetails.getUser();
         } else {
-            throw new RuntimeException("Principal is not an instance of UserDetailsImpl");
+            throw new UnauthorizedException("Invalid authentication principal");
         }
     }
 }
-
