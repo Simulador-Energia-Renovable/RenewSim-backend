a49478d642605e2fc6492e342fdbee29bc42384e
lannyRivero <lannyrivero288@gmail.com>
2025-04-09 22:41:40 +0200

    feat(profile): create DTOs for profile creation, update and response

--- src/main/java/com/renewsim/backend/profile/dto/UpdateProfileRequestDTO.java
+++ src/main/java/com/renewsim/backend/profile/dto/UpdateProfileRequestDTO.java
@@ -1,17 +1,15 @@
 package com.renewsim.backend.profile.dto;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
-public record UpdateProfileRequestDTO() {
+public class UpdateProfileRequestDTO {
     private String firstName;
     private String lastName;
     private String email;
-    private String phone; 
+    private String phone;  
     private String profileImageUrl;
 
 }
