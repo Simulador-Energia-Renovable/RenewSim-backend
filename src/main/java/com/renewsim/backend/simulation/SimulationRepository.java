ba89e41feada2f09316feb30e3dade1a4eb8e4b2
lannyRivero <lannyrivero288@gmail.com>
2025-04-16 22:32:45 +0200

    refactor(repository): reorder imports in SimulationRepository for consistency

--- src/main/java/com/renewsim/backend/simulation/SimulationRepository.java
+++ src/main/java/com/renewsim/backend/simulation/SimulationRepository.java
@@ -1,9 +1,10 @@
 package com.renewsim.backend.simulation;
 
-import org.springframework.stereotype.Repository;
 import com.renewsim.backend.user.User;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SimulationRepository extends JpaRepository<Simulation, Long> {
