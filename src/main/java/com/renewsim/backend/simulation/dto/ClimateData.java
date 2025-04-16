f6c2e1f655d70e91e4a57f932d3f0d519944bc74
lannyRivero <lannyrivero288@gmail.com>
2025-04-16 21:44:41 +0200

    refactor(dto): remove redundant getters and setters from ClimateData using Lombok

--- src/main/java/com/renewsim/backend/simulation/dto/ClimateData.java
+++ src/main/java/com/renewsim/backend/simulation/dto/ClimateData.java
@@ -1,38 +1,14 @@
 package com.renewsim.backend.simulation.dto;
 
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode
 public class ClimateData {
-    private double irradiance;    
+    private double irradiance;
     private double wind;
     private double hydrology;
-
-    public double getWind() {
-        return wind;
-    }
-
-    public void setWind(double wind) {
-        this.wind = wind;
-    }
-  
-
-    public double getHydrology() {
-        return hydrology;
-    }
-
-    public void setHydrology(double hydrology) {
-        this.hydrology = hydrology;
-    }
-
-    public double getIrradiance() {
-        return irradiance;
-    }
-
-    public void setIrradiance(double irradiance) {
-        this.irradiance = irradiance;
-    }
 }
 
