561a2feb52d3a7bcb9fbb23d537028553034956f
lannyRivero <lannyrivero288@gmail.com>
2025-04-18 13:37:19 +0200

    fix(controller): update API endpoint to include versioning in TechnologyComparisonController

--- src/main/java/com/renewsim/backend/technologyComparison/TechnologyComparisonController.java
+++ src/main/java/com/renewsim/backend/technologyComparison/TechnologyComparisonController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/technologies")
+@RequestMapping("/api/v1/technologies")
 public class TechnologyComparisonController {
 
     private final TechnologyComparisonUseCase useCase;
